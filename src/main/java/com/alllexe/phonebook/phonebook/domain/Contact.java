17773f988ddca3615121dafe8bd4455acec0e580
alabra <alllexe@mail.ru>
2020-02-14 21:36:56 +0300

    added swagger

---
+++ src/main/java/com/alllexe/phonebook/phonebook/domain/Contact.java
@@ -0,0 +1,65 @@
+/**
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 14.02.2020
+ */
+package com.alllexe.phonebook.phonebook.domain;
+
+import java.util.Objects;
+
+public class Contact {
+
+    private Integer id;
+    private String name;
+    private String phone;
+
+    public Contact() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(id, contact.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
